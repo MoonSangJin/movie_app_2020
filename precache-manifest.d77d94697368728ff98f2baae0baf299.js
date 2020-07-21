a50c91032410830932f8d2df7c324d3ef561c9aa
MoonSangJin <1128msj@naver.com>
2020-07-21 11:33:57 +0900

    Updates

---
+++ precache-manifest.d77d94697368728ff98f2baae0baf299.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d87d2189186e7e28445c9f52f875735",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "8515d34ee3e601ce0da9",
+    "url": "/movie_app_2020/static/css/main.05d37451.chunk.css"
+  },
+  {
+    "revision": "b4ad6d5796d4c9c3f3c1",
+    "url": "/movie_app_2020/static/js/2.3507762e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.3507762e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8515d34ee3e601ce0da9",
+    "url": "/movie_app_2020/static/js/main.928c0c32.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
