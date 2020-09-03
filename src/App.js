2cfeb9e1901258fa107c08ac07077d2fb094c055
MoonSangJin <1128msj@naver.com>
2020-09-03 18:57:09 +0900

    Adding Description Comment

--- src/App.js
+++ src/App.js
@@ -13,25 +13,25 @@ class App extends React.Component {
         data: { movies }
       }
     } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
-    this.setState({ movies, isLoading: false });
+    this.setState({ movies, isLoading: false }); //url에 있는 무비 다 가져옴
     console.log(movies);
   }
   componentDidMount() {
-    this.getMovies();
+    this.getMovies(); 
 
   }
 
   render() {
     const { isLoading, movies } = this.state;
     return( 
-    <section className="container">
-      {isLoading ? (
+    <section className="container"> 
+      {isLoading ? ( //로딩중이면 isLoading이 true니까 "Loading" 띄우고
       <div className="loader">
         <span className="loader__text">Loading...</span>
       </div>
-      ): (
+      ): ( //다 끝나서 componentDidMount가 실행되서 setState통해 isLoading이 false로 바뀌면
     <div className="movies">
-      {movies.map(movie => (
+      {movies.map(movie => ( //Movie 컴포넌트 실행
         <Movie 
         key={movie.id}
         id={movie.id} 
--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render( <App/>,
-  document.getElementById('potato')
+  document.getElementById('potato') //potato라는 id가진 html공간에 App component를 그리겠다.
 );
